30c9c8b9a2b57e4d4d37d1041390710b53a83ac0
Takahashi <b1016140@gmail.com>
2018-11-21 12:55:06 +0900

    move range

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	Vector2 velocity;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb=GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity.x=Input.GetAxisRaw("Horizontal")*3.0f;
+		velocity.y=Input.GetAxisRaw("Vertical")*3.0f;
+	}
+	void FixedUpdate(){
+		rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	}
+}
