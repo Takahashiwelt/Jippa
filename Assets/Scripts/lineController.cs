8a53d9e77b6a221716c577c9827c149fcae03baf
Takahashi <b1016140@gmail.com>
2018-11-22 20:36:21 +0900

    11/22

---
+++ Assets/Scripts/lineController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineController : MonoBehaviour {
+
+	public GameObject lineObject;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			Instantiate(lineObject,new Vector2(transform.position.x,transform.position.y),Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/Scripts/lineObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineObject : MonoBehaviour {
+	
+	public GameObject LineObject;
+	private LineRenderer lineRenderer;
+	private int lineIndex=1;
+
+	private bool control=true;
+	// Use this for initialization
+	void Start () {
+		lineRenderer=GetComponent<LineRenderer>();
+		Debug.Log(lineRenderer);
+		lineRenderer.enabled=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)&&control){
+			touch();
+		}
+		if(Input.GetMouseButtonUp(0)){
+			control=false;
+		}
+
+	}
+	void touch(){
+		Vector2 screenPoint=Input.mousePosition;
+		Vector2 worldPoint=Camera.main.ScreenToWorldPoint(screenPoint);
+		Debug.Log(worldPoint);
+
+		lineRenderer.enabled=true;
+		lineRenderer.SetVertexCount(lineIndex);
+		lineRenderer.SetPosition(lineIndex-1,worldPoint);
+		lineIndex++;
+	}
+}
